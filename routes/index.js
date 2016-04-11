e8d987e948ba9de386b785cecfe18d08b8bd165f
Jennifer Bland <ratracegrad@gmail.com>
2016-04-11 09:32:08 -0400

    Updated index route to show all polls. Added route to show poll details

--- routes/index.js
+++ routes/index.js
@@ -37,14 +37,38 @@ module.exports = function(app, passport) {
     }));
 
     app.get('/profile', isLoggedIn, function(req, res) {
+        pollModel.find({}, function(err, doc) {
+            if (err) {
+                res.status(400).json({error: err});
+            }
 
-        res.render('profile', {user: req.user});
+            res.render('profile', {user: req.user,
+                                    polls: doc});
+        });
+
     });
 
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
+
+    app.post('/newpoll', function(req, res) {
+        var entry = new pollModel(req.body);
+        entry.save(function(err, doc) {
+            if (err) {
+                return res.status(400).json({error: err});
+            }
+
+            res.json(doc);
+        });
+    });
+
+    app.get('/polldetails/:poll', function(req, res) {
+        var params = req.params.poll;
+
+    })
+
 }
 
 function isLoggedIn(req, res, next) {
