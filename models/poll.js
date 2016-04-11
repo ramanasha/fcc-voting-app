0e2f5e8e66a8c0b8cb11605f7e066ddc2bebdb00
Jennifer Bland <ratracegrad@gmail.com>
2016-04-10 22:36:28 -0400

    Added models for user and polls

--- models/poll.js
+++ models/poll.js
@@ -1,3 +1,12 @@
-/**
- * Created by jbland on 4/10/16.
- */
+var mongoose= require('mongoose');
+
+var pollSchema = mongoose.Schema({
+                                     name:      String,
+                                     entries:   Number,
+                                     choices:   [Array],
+                                     email:     String
+                                 });
+
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Poll', pollSchema);
